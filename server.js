d0cee37a092d8ae8c0d803cece882787f3f79c35
AriWebDev <88588582+AriWebDev@users.noreply.github.com>
2021-08-09 00:57:32 +0300

    Updated server.js, Procfile

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV === 'production'){
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   );
   
   app.use((err, req, res, next) => {
