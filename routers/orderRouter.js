611ecf7fdc6671d375887e0752fd614fb692775c
AriWebDev <88588582+AriWebDev@users.noreply.github.com>
2021-08-08 23:44:36 +0300

    Added some changes in server file

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
-import { isAdmin, isAuth, isSellerOrAdmin } from '../utils.js';
+import { isAdmin, isAuth, isSellerOrAdmin } from '../utils.js'
 
 const orderRouter = express.Router();
 
